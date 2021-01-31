e982054bd6150ad9cacae9a5813afbb78ebe2408
Lesley-Middelkamp <73035796+Lesley-Middelkamp@users.noreply.github.com>
2021-01-31 17:40:34 -0500

    test

---
+++ src/Temperature.js
@@ -0,0 +1,19 @@
+import React from "react";
+import temperature from "./temperature.css";
+
+export default function Temperature(){
+  return(
+    <div className="Temperature">
+<h2> The temperature is 3℃</h2>
+<br/>
+<h2> Friday 10:28 
+  <br/>
+  10℃
+  <br />
+Mostly cloudy
+<br />
+Sunny skies all day today.
+</h2>
+</div>
+  )
+}
