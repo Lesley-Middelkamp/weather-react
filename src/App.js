362661fb6163689c49a359cc03fe9b4a4756b2a1
Lesley-Middelkamp <73035796+Lesley-Middelkamp@users.noreply.github.com>
2021-02-03 20:18:08 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Temperature from "./Temperature";
 import CelFarh from "./CelFarh";
 import Humwind from "./Humwind";
 import Sourcecode from "./Sourcecode";
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <Search />
       <Temperature />
       <CelFarh />
       <Humwind />
       <Sourcecode />
+    </div>
     </div>
   );
 }
